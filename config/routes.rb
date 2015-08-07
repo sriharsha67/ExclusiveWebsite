81a6798d82cd61fc031d6850e9d16af8e2c0c52c
sriharsha67 <sriharsha67@gmail.com>
2015-08-07 16:40:58 +0530

    Added timer anduser can join waiting list

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+ get 'waiting' => 'home#waiting', as: :waiting
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
